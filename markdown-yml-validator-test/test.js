05397d937dbe114d7c83e6550c301c019b73404e
Akib Mohaimenur Rahman <akib.rhast@gmail.com>
2021-03-02 18:35:33 -0500

    validate guide collection

---
+++ markdown-yml-validator-test/test.js
@@ -0,0 +1,36 @@
+var fs = require('fs')
+  , fm = require('front-matter')
+
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+var schema = {
+  "id": "/GuidesSchema",
+  "type": "object",
+  "properties": {
+    "title": {"type": "string"},
+    "description": {"type": "string"},
+    "status": {
+      "type": "string",
+      "enum": ["active", "inactive"]
+    },
+    "display": { "type": "boolean" }
+
+  },
+  "required":["title","description","status","display"]
+};
+
+fs.readFile('test_files/t2.md', 'utf8', function(err, data){
+  if (err) throw err
+ 
+  var {attributes} = fm(data)
+  let x = v.validate(attributes, schema)
+  if(x.errors.length > 0){
+    console.log(x.errors) 
+  }
+  else{
+    console.log(attributes);
+  }
+
+  
+})
+
